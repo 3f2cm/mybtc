be11e618851e8aae5044c50f71fe2ccd607e7af6
_3F_2 cm <3f2cm@users.noreply.github.com>
2023-01-18 23:25:11 -0800

    Modify the way to find WIF for ScriptPubKey to use PubKeyHash for tx generate
    The current logic assumes the WIF and the ScriptPubKey are tightly bound
    in the input format. However, the ScriptPubKey includes PubKeyHash, and
    it's possible to find WIF that has matching PubKey.
    
    This commit will change the logic for `tx generate` subcommand to use
    PubKeyHash, and also modify the input format to follow the change.

--- tx/tx.go
+++ tx/tx.go
@@ -24,7 +24,6 @@ type In struct {
 	TxID         string `json:"txid"`
 	Vout         uint32 `json:"vout"`
 	ScriptPubKey string `json:"scriptpubkey"`
-	WIF          string `json:"wif"`
 }
 
 // Out contains necessary info to establish transaction message's TxOut items.
@@ -35,9 +34,13 @@ type Out struct {
 
 // Input expresses an input to generate command that build transaction message with signatures.
 type Input struct {
-	Ins  []In  `json:"in"`
-	Outs []Out `json:"outs"`
+	Ins  []In     `json:"in"`
+	Outs []Out    `json:"outs"`
+	WIFs []string `json:"wifs"`
 }
+
+// wifDB stores WIFs with keys of their public key hashes.
+type wifDB map[string]*btcutil.WIF
 
 var (
 	errEmptyTxIn  = errors.New("there are no TxIn items in the input")
@@ -63,13 +66,25 @@ func Generate(b []byte) ([]byte, error) {
 	// Initialize msgTx to construct it and insert a signature into its TxIn
 	msgTx := wire.NewMsgTx(wire.TxVersion)
 
-	// Construct msgTx.TxOut and msgTx.TxIn
+	// Construct msgTx.TxOut
 	if err := addOutToTx(msgTx, input.Outs); err != nil {
 		return nil, fmt.Errorf("couldn't construct TxOut for msgTx: %w", err)
 	}
 
+	// Construct msgTx.TxIn
 	if err := addInToTx(msgTx, input.Ins); err != nil {
 		return nil, fmt.Errorf("couldn't construct TxIn for msgTx: %w", err)
+	}
+
+	// Decode WIFs in the input
+	wdb, err := decodeWIFs(input.WIFs)
+	if err != nil {
+		return nil, fmt.Errorf("couldn't decode WIFs in the input: %w", err)
+	}
+
+	// Add signatures to msgTx.TxIn
+	if err := updateSignatures(msgTx, input.Ins, wdb); err != nil {
+		return nil, fmt.Errorf("couldn't sign msgTx: %w", err)
 	}
 
 	// Serialize and hexdump the msgTx with signs
@@ -112,27 +127,72 @@ func addInToTx(t *wire.MsgTx, ins []In) error {
 		t.AddTxIn(txIn)
 	}
 
-	// Add a signature to each TxIn in msgTx.TxIn
-	for i, txin := range ins {
-		// Decoding WIF to use its private key
-		wif, err := btcutil.DecodeWIF(txin.WIF)
+	return nil
+}
+
+func decodeWIFs(wifs []string) (wifDB, error) {
+	w := make(wifDB)
+
+	for _, encWIF := range wifs {
+		wif, err := btcutil.DecodeWIF(encWIF)
 		if err != nil {
-			return fmt.Errorf("couldn't decode given wif: %w", err)
+			return nil, fmt.Errorf("couldn't decode given WIF: %w", err)
 		}
 
-		// Construct a signature
-		prevPubKeyScript, err := hex.DecodeString(txin.ScriptPubKey)
+		pubKey := wif.SerializePubKey()
+		pubKeyHash := btcutil.Hash160(pubKey)
+		pubKeyHashEnc := hex.EncodeToString(pubKeyHash)
+		w[pubKeyHashEnc] = wif
+	}
+
+	return w, nil
+}
+
+func updateSignatures(t *wire.MsgTx, ins []In, wdb wifDB) error {
+	// Add a signature to each TxIn in msgTx.TxIn
+	for i, txin := range ins {
+		// Parse the public key script in the input
+		prevPubKeyScriptBytes, err := hex.DecodeString(txin.ScriptPubKey)
 		if err != nil {
 			return fmt.Errorf("couldn't decode the previous public key script: %w", err)
 		}
 
-		signature, err := txscript.SignatureScript(t, i, prevPubKeyScript, txscript.SigHashAll, wif.PrivKey, false)
+		// Extract the PubKey hash from the public key script
+		prevPubKeyScript, err := txscript.ParsePkScript(prevPubKeyScriptBytes)
+		if err != nil {
+			return fmt.Errorf("couldn't parse the given public key script: %w", err)
+		}
+
+		pubKeyHash := extractPubKeyHash(prevPubKeyScript.Script())
+		if pubKeyHash == nil {
+			return fmt.Errorf("couldn't generate an address from the public key script: %w", err)
+		}
+
+		// Find the WIF corresponding to the above PubKey hash from the WIF list in the input
+		wif, ok := wdb[hex.EncodeToString(pubKeyHash)]
+		if !ok {
+			return fmt.Errorf("couldn't find WIF corresponding to the public key script %s in given WIFs", txin.ScriptPubKey)
+		}
+
+		// Construct a signature
+		signature, err := txscript.SignatureScript(t, i, prevPubKeyScriptBytes, txscript.SigHashAll, wif.PrivKey, false)
 		if err != nil {
 			return fmt.Errorf("couldn't generate a signature for the tx: %w", err)
 		}
 
 		// Add the constructed signature to the corresponding TxIn
 		t.TxIn[i].SignatureScript = signature
+	}
+
+	return nil
+}
+
+// stolen from https://github.com/btcsuite/btcd/blob/1d77730e9a92eafadb9f1ef86d2522c36ca4db38/txscript/standard.go#L154
+func extractPubKeyHash(script []byte) []byte {
+	if len(script) == 25 &&
+		script[0] == txscript.OP_DUP && script[1] == txscript.OP_HASH160 && script[2] == txscript.OP_DATA_20 &&
+		script[23] == txscript.OP_EQUALVERIFY && script[24] == txscript.OP_CHECKSIG {
+		return script[3:23]
 	}
 
 	return nil
